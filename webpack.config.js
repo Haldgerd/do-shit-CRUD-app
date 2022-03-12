64dcb339b14099a25c9a3b5f42bb478457106de3
Haldgerd <haldgerd.code@gmail.com>
2022-03-12 06:26:11 +0100

    "Add asset configuration for images to webpack config, add typography and variables stylesheet, add script to package.json."

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,58 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const setConfiguration = (env) => {
-  // source mapping
-  const isProduction = env === 'production';
+module.exports = (env) => {
+    // source mapping
+    const isProduction = env === 'production';
 
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, './build/'),
-      publicPath: '/',
-      filename: 'bundle.js',
-    },
-    resolve: {
-      alias: {
-        components: path.resolve(__dirname, './src/components'),
-      },
-      extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-      // contentBase: path.join(__dirname, './build'),
-      static: './',
-      compress: true,
-      port: 3000,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader', 'eslint-loader'],
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, './dist/'),
+            publicPath: './dist/',
+            filename: 'bundle.js/',
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            'style-loader',
-            // Translates CSS into CommonJS
-            'css-loader',
-            // Compiles Sass to CSS
-            'sass-loader',
-          ],
+        resolve: {
+            alias: {
+                components: path.resolve(__dirname, './src/components/'),
+            },
+            extensions: ['.js', '.jsx'],
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve('./public/index.html'),
-      }),
-    ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-  };
+        devServer: {
+            // contentBase: path.join(__dirname, './build'),
+            static: './',
+            compress: true,
+            port: 3000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'eslint-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|jpg)$/,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve('./public/index.html'),
+            }),
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+    };
 };
-
-export default setConfiguration;
